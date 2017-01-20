24f2b5859e539dd86d2ae79137b78f934813f5a0
ryotako <kobayashiryota2046@gmail.com>
2017-01-20 16:24:50 +0900

    Add sublist.fish

---
+++ functions/sublist.fish
@@ -0,0 +1,13 @@
+function sublist
+  while read -a line
+    set -l lst 1
+    while test $lst -le (count $line)
+      set -l fst 1
+      while test $fst -le $lst
+        echo $line[$fst..$lst]
+        set fst (math $fst + 1)
+      end
+      set lst (math $lst + 1)
+    end
+  end
+end
