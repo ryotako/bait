a39d115c273410ff3238131d91ebdba417c3db2a
ryotako <kobayashiryota2046@gmail.com>
2017-01-22 00:36:02 +0900

    add nest*.fish

---
+++ functions/nestr.fish
@@ -0,0 +1,15 @@
+function nestr
+  function __nestr
+    read -al line
+    if test (count $line) -lt 2
+      string replace \* " $line[1] " "$argv[1]"
+    else
+      set -l repl (echo $line[2..-1] | __nestr $argv)
+      string replace \* " $line[1] $repl " "$argv[1]"
+    end
+  end
+
+  while read line
+    echo $line | __nestr $argv
+  end
+end
