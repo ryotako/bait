9e94abd741c2667ae9d4ee6b9d8b605771a587f3
ryotako <kobayashiryota2046@gmail.com>
2017-01-21 15:56:31 +0900

    Add cycle.fish

---
+++ functions/cycle.fish
@@ -0,0 +1,14 @@
+function cycle
+  while read -a line
+    set -l i 1
+    while test $i -le (count $line)
+      if test $i -eq 1
+        echo $line
+      else
+        set -l i_ (math $i - 1)
+        echo $line[$i..-1] $line[1..$i_]
+      end
+      set i (math $i + 1)
+    end
+  end
+end
