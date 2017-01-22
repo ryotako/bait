d3cc9b32916ca81ffb9d2e16b66eca8f77acc42a
ryotako <kobayashiryota2046@gmail.com>
2017-01-22 15:40:10 +0900

    add option parse

--- bait.fish
+++ bait.fish
@@ -1,11 +1,108 @@
 function bait -d 'controlling records and fields given by particular separators'
+    # declare variables
+    set -l arg
+    set -l opt_fs
+    set -l opt_ifs
+    set -l opt_ofs
+    set -l opt_eor
+    set -l opt_eos
+    set -l opt_each 0
 
-    switch "$argv[1]"
-        case -h --help
-            __bait_help
+    # parse arguments
+    while count $argv > /dev/null
+        switch $argv[1]
+            case --
+                set arg $arg $argv[2]
+                set -e argv[1..2]
+
+            case -h --help
+                __bait_help
+                return
+
+            case --fs
+                set -e argv[1]
+                if count $argv > /dev/null
+                    set opt_fs $argv[1]
+                    set -e $argv[1]
+                end
+
+            case --ifs
+                set -e argv[1]
+                if count $argv > /dev/null
+                    set opt_ifs $argv[1]
+                    set -e $argv[1]
+                end
+
+            case --ofs
+                set -e argv[1]
+                if count $argv > /dev/null
+                    set opt_ofs $argv[1]
+                    set -e $argv[1]
+                end
+
+            case --eor
+                set -e argv[1]
+                if count $argv > /dev/null
+                    set opt_eor $argv[1]
+                    set -e $argv[1]
+                end
+
+            case --eos
+                set -e argv[1]
+                if count $argv > /dev/null
+                    set opt_eos $argv[1]
+                    set -e $argv[1]
+                end
+
+            case --each
+                set -e argv[1]
+                set opt_each 1
+
+            case -\*\?
+                printf "bait: '%s' is not a valid option\n" $argv[1] >/dev/stderr
+                return 1
+
+            case \*
+                if test -z $arg
+                    set arg $argv[1]
+                    set -e argv[1]
+                else
+                    echo "bait: too many arguments" >/dev/stderr
+                    return 1
+                end
+        end
     end
 
+    # set dafault values of options
+    if test -z opt_fs
+        set opt_fs ' '
+    end
+
+    if test -z opt_ifs
+        set opt_if tests $opt_fs
+    end
+
+    if test -z opt_ofs
+        set opt_ofs $opt_fs
+    end
+
+    if test -z opt_eor
+        set opt_eor \n
+    end
+
+    if test -z opt_eos
+        set opt_eos \n
+    end
+
+    printf "ARG: %s\n" $argv[1]
+    echo opt_fs   $opt_fs
+    echo opt_ifs  $opt_ifs
+    echo opt_ofs  $opt_ofs
+    echo opt_eor  $opt_eor
+    echo opt_eos  $opt_eos
+    echo opt_each $opt_each
 end
+
 
 function __bait_help
     echo "Usage: bait [COMMAND] [OPTIONS] [N or STR]"
